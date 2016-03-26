e10558961f73edfdbf03043fcaad4ae676a27abb
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-26 14:01:12 -0700

    playerOne.drive function doesn't work when it definitely should. WHYYY???

--- app.js
+++ app.js
@@ -8,21 +8,37 @@ $(document).on('ready', function() {
     var template = Handlebars.compile(source);
 
     /*Create player object and run through template*/
-    var playerOne = new Player(0, "hotpink");
-    var playerTwo = new Player(0, "turquoise");
+    var playerOne = new Player("player-one", 0, "hotpink", 48);
+    var playerTwo = new Player("player-two", 0, "turquoise", 49);
     var playersArray = [playerOne, playerTwo];
-    console.log(playersArray);
+
 
     var playerHTML = template({ players: playersArray });
     console.log(playerHTML);
 
+
     $('.playerEntry').append(playerHTML);
 
+    //Function for player to drive
+    // playerOne.drive();
+    // playerTwo.drive();
+    $(window).on('keydown', function moveRight(event) {
+      if (event.keyCode === 48) {
+        playerOne.drive();
+      } else if (event.keyCode === 49) {
+        playerTwo.drive();
+      }
+    });
 
 });
 
-function Player(wins, color) {
-  //this.name = name;
+function Player(id, wins, color, keyButton) {
+  this.id = id;
   this.wins = wins;
   this.color = color;
+  this.keyButton = keyButton;
+  //Function below doesn't work and I don't know why
+  this.drive = function () {
+        $('"#' + id + '"').animate({"left": "+=10px" }, 0);
+  };
 }
